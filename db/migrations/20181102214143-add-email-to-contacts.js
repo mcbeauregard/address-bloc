27a2db125ab31e4adcd1b11bfc80b77158d21f6a
mcbeauregard <mcbeauregard@gmail.com>
2018-11-02 14:56:49 -0700

    assign. questions one and two complete

---
+++ db/migrations/20181102214143-add-email-to-contacts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("Contacts", "email" { //first arg is name of table, second is object of key-value pairs
+      id: {   
+        type: Sequelize.INTEGER //sets property type. Only values of this type are allowed
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {  // We call the down method when we roll back the migration, and it undoes whatever the up method did. In this case, it drops the Contacts table
+    return queryInterface.deleteColumn('Contacts', 'email');
+  }
+};
