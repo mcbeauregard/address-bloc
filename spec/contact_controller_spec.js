58fb9900d7ca290aa92d14d8b7af6e7e5c569d04
mcbeauregard <mcbeauregard@gmail.com>
2018-11-02 15:23:03 -0700

    modified spec/contact_controller_spec.js to include email

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -21,12 +21,14 @@ describe("ContactController", () => {
 
     // pass argument where needed via done
     it("should add a single contact into the book", (done) => {
-        this.book.addContact("Alice", "001-101-1010") // We call addContact on book, and it returns a promise. When resolved, we execute the callback where we set our expectations. When we successfully POST to the database, the ORM returns the object it just persisted so we can confirm it was successful.
+        this.book.addContact("Alice", "001-101-1010","alice@bloc.io") // We call addContact on book, and it returns a promise. When resolved, we execute the callback where we set our expectations. When we successfully POST to the database, the ORM returns the object it just persisted so we can confirm it was successful.
         .then((contact) => {
     
             // We have access to the return object inside of the parameter defined on the callback. We called it contact since it should represent a contact object. We confirm that the attributes we passed in have been added to the contact object. We want to call done at the end of our test to signal Jasmine that the test is finished and the async call is completed.        
             expect(contact.name).toBe("Alice");
             expect(contact.phone).toBe("001-101-1010");
+            // returns email
+            expect(contact.email).toBe("alice@bloc.io");
             done();
           })
           .catch((err) => {
