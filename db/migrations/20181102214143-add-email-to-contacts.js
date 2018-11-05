ea669713dc2faeb7a15268495ef688b1a779c3a2
mcbeauregard <mcbeauregard@gmail.com>
2018-11-04 17:48:21 -0800

    fixed syntax, and switch integer to string in migration email file

--- db/migrations/20181102214143-add-email-to-contacts.js
+++ db/migrations/20181102214143-add-email-to-contacts.js
@@ -2,14 +2,12 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn("Contacts", "email" { //first arg is name of table, second is object of key-value pairs
-      id: {   
-        type: Sequelize.INTEGER //sets property type. Only values of this type are allowed
-      }
-    });
+    return queryInterface.addColumn('Contacts', 'email', { //first arg is name of table, second is object of key-value pairs
+        type: Sequelize.STRING //sets property type. Only values of this type are allowed
+      });
   },
 
   down: (queryInterface, Sequelize) => {  // We call the down method when we roll back the migration, and it undoes whatever the up method did. In this case, it drops the Contacts table
-    return queryInterface.deleteColumn('Contacts', 'email');
+    return queryInterface.deleteColumn('Contacts','email');
   }
 };
